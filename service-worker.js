9083007002f8214385cf1c37cb5f370c93e449a6
DiogoPCastelos <main@diogopcastelos.pt>
2025-01-31 13:43:16 +0000

    Updates

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/offline.html",
+        "/images/favicon.png", // Cache static assets
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => caches.match("/offline.html"))
+  );
+});
