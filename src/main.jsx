1418735d5fe64ac7557a72dee333591f3478d936
DiogoPCastelos <main@diogopcastelos.pt>
2025-02-01 00:25:35 +0000

    Revert "Fix - minor routing issues"
    This reverts commit 68707c96b3edbaaab1b859ad10aa28dccbc7b7b5.

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import HomePage from "./routes/HomePage";
 
 // Add the `basename` for deployment under /websiteReact
 const router = createBrowserRouter(
-  [
-    { path: "/", element: <HomePage /> },
-    { path: "*", element: <Navigate to="/" replace /> }, // 🔄 Redirects properly
-  ],
-  { basename: "" }
+  [{ path: "/", element: <HomePage /> }],
+  { basename: "" } // This is where you set the base path
 );
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/service-worker.js")
+      .then((reg) => console.log("Service Worker registered!", reg))
       .catch((err) =>
         console.error("Service Worker registration failed:", err)
       );
-    console.log(
-      "What are you looking for? Stop looking at the console and just enjoy the website. Yes, image disposition is random in the projects section, yes it is made with react, yes it loads fast, yes the images are webp, yes it's pwa compliant, yes there are too many animations to count, yes most of them are on Math.random() timers, yes it even has a service worker.\n\n If you're a hiring manager, yes, you should hire me."
-    );
   });
 }
 
