661cf543a9317935a26692ccb57c021f8fde3574
DiogoPCastelos <main@diogopcastelos.pt>
2025-03-18 08:45:14 +0000

    Updated - removed service worker (also added check to ensure potential cached service workers are removed)

--- src/main.jsx
+++ src/main.jsx
@@ -11,18 +11,17 @@ const router = createBrowserRouter(
 );
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(
-        () =>
-          "What are you looking for? Stop looking at the console and just enjoy the website. Yes, image disposition is random in the projects section, yes it is made with react, yes it loads fast, yes the images are webp, yes it's pwa compliant, yes there are too many animations to count, yes most of them are on Math.random() timers, yes it even has a service worker.\n\n If you're a hiring manager, yes, you should hire me."
-      )
-      .catch((err) =>
-        console.error("Service Worker registration failed:", err)
-      );
-    console.log;
-  });
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister();
+        console.log("Service worker unregistered");
+      });
+    })
+    .catch((error) => {
+      console.error("Error unregistering service workers:", error);
+    });
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
