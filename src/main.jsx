3bb5ae6a7aeaaa1332a906f0f98ffd7f8caab545
DiogoPCastelos <main@diogopcastelos.pt>
2025-05-06 16:45:18 +0100

    Add - Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,28 @@ const router = createBrowserRouter(
   { basename: "" } // This is where you set the base path
 );
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDN1QLdt2nfT8EiTeFwmOJbBWQGmGSFWhw",
+  authDomain: "websitereact-cd7e9.firebaseapp.com",
+  projectId: "websitereact-cd7e9",
+  storageBucket: "websitereact-cd7e9.firebasestorage.app",
+  messagingSenderId: "629387179127",
+  appId: "1:629387179127:web:678483370ad04faa688272",
+  measurementId: "G-7XD00JQPCT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 console.log(
   "Hi, why are you looking at the console? Nothing to see here! (hopefully?)\nHope you're having a great day! 😄"
 );
